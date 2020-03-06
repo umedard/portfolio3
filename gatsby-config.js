ecff960973e90b224258619808ce089a677f71b2
Medard <umedard@gmail.com>
2020-03-06 23:30:22 +0100

    config settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Medard Urban`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@medardurban`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Medard Urban`,
+        short_name: `Medard urban`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,

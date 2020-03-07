026d53d294b74f16df3e39f70af16ba43a2bffab
Medard <umedard@gmail.com>
2020-03-07 21:47:43 +0100

    add portfolio

--- src/components/portfolioItem.js
+++ src/components/portfolioItem.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import style from './portfolioItem.module.css'
 
-export default function PortfolioItem() {
+
+export default function PortfolioItem({picture, title, description, github, url}) {
     return (
-        <section>
+        <section className={style.portfolio}>
+            <div className={style.portfolio__picture}>
+                <div className={style.hover}>
+                      
+                </div>
+            </div>
+            <div className={style.portfolio__title}>{title}</div>
+            <div className={style.portfolio__description}>{description}</div>
             
         </section>
     )
 }
+
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from "react"
 
 import Layout from "../components/layout"
-
+import PortfolioItem from '../components/portfolioItem'
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Projects" />
-    Projects
+    <PortfolioItem picture="./img/portfolio.png" title="Car company website" description="CSS, vanilla JavaScript" github="" url=""/>
+    <PortfolioItem picture="./img/emei.png" title="Car company website" description="CSS, vanilla JavaScript" github="" url=""/>
   </Layout>
 )
 

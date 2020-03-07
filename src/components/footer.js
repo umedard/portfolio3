de4a09b0f6a4c0cadfabe4b577c3d1f3a39411e8
Medard <umedard@gmail.com>
2020-03-07 11:34:02 +0100

    add background image to foooter

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,14 @@ import style from './footer.module.css'
 
 export default function Footer() {
     return (
-        <footer className={style.footer}>
+        <footer>
+            <div className={style.footer__wrapper}>
+                <div>
+                    <div className={style.footer__messsage}>Say hi!</div>
+                    <div className={style.footer__email}>umedard@gmail.com</div>
+                </div>
+                
+            </div>
             
             <div className={style.footer__copyright}>Footer</div>
             

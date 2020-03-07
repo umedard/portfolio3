687099db2f5184e66e7d085083397a8c11af65b1
Medard <umedard@gmail.com>
2020-03-07 13:23:36 +0100

    add font source sans pro

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
       options: {
         fonts: [
           `Bree Serif`,
-          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+          `source sans pro` // you can also specify font weights and styles
         ],
         display: 'swap'
       }

574de80af851baee13cf6e5def7aee01bd52fe2f
Medard <umedard@gmail.com>
2020-06-16 22:08:38 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,8 @@ const Index = () => (
     <SEO title="Projects" />
     <Portfolio>
        <PortfolioItem picture="/img/emei.png" title="Bristol cars" description="CSS, vanilla JavaScript, Netlify" github="https://github.com/umedard/bristol" url="https://bristol-medard.netlify.com/"/>
-       <PortfolioItem picture="/img/bristol.png" title="Portfolio" description="Gatsby, Module CSS, Netlify" github="https://github.com/umedard/portfolio3" url="https://medard.me"/>
-       <PortfolioItem picture="/img/portfolio.png" title="Emei" description="Gatsby, Netlify CMS, Markdown" github="https://github.com/umedard/emei" url="https://emei-medard.netlify.com/"/>
+       <PortfolioItem picture="/img/bristol.png" title="Portfolio" description="React, Gatsby, Module CSS, Netlify" github="https://github.com/umedard/portfolio3" url="https://medard.me"/>
+       <PortfolioItem picture="/img/portfolio.png" title="Emei" description="React, Gatsby, Netlify CMS, Markdown" github="https://github.com/umedard/emei" url="https://emei-medard.netlify.com/"/>
     </Portfolio>
   </Layout>
 )

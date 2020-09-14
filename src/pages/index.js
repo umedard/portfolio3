87893aaca1c4f570b748479cb25fecbde60ace58
Medard <umedard@gmail.com>
2020-09-14 17:26:54 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ const Index = () => (
        <PortfolioItem picture="/img/emei.png" title="Bristol cars" description="CSS, vanilla JavaScript, Netlify" github="https://github.com/umedard/bristol" url="https://bristol-medard.netlify.com/"/>
        <PortfolioItem picture="/img/bristol.png" title="Portfolio site" description="React, Gatsby, Module CSS, Netlify" github="https://github.com/umedard/portfolio3" url="https://medard.dev"/>
        <PortfolioItem picture="/img/portfolio.png" title="Emei" description="React, Gatsby, Netlify CMS, Markdown" github="https://github.com/umedard/emei" url="https://emei-medard.netlify.com/"/>
+       <PortfolioItem picture="/img/portfolio.png" title="Aurora Box" description="HTML, CSS, SASS" github="https://github.com/umedard/aurorabox" url="https://aurorabox.netlify.app/"/>
     </Portfolio>
   </Layout>
 )
